d3c92b78b9dab328dd97e53127457731b86efffd
Ashwin Gudepu <Ashwin_Gudepu@epam.com>
2018-01-09 17:29:11 +0530

    Business Objects Tests

---
+++ src/main/java/drivers/ChromeBrowserIntializer.java
@@ -0,0 +1,28 @@
+package drivers;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserIntializer extends BrowserIntializer {
+	final static Logger logger = Logger.getLogger(ChromeBrowserIntializer.class);
+	@Override
+	public WebDriver intializeBrowser() {
+		if (null == driver) {
+			BrowserProperties.Builder builder = new BrowserProperties.Builder();
+			builder.browserClassPath("webdriver.chrome.driver").browserDriverPath(".//src//main//resources//chromedriver.exe");
+			BrowserProperties chromeProperties = builder.build();
+			System.setProperty(chromeProperties.getbrowserClassPath(), chromeProperties.getbrowserDriverPath());
+			driver = new ChromeDriver();
+		}
+		logger.info("Chrome Browser Initiated");
+		return driver;
+	}
+
+	public static void closeWebBrowser() {
+		if (null != driver) {
+			driver.quit();
+		}
+		driver = null;
+	}	
+}
